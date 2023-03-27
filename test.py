a3bd55399c7bc55347e4733548a7fb013d6929f0
Ekaterina <108363825+mckater@users.noreply.github.com>
2023-03-27 20:55:24 +0300

    stage_1

---
+++ test.py
@@ -0,0 +1,25 @@
+import pprint
+import json
+from requests import get, post, delete, put
+
+# ff = open('response.json')
+# Response: Говорит Алиса.
+# pprint.pprint(post('http://127.0.0.1:5000/post', json=json.load(ff)).json())
+# ff.close()
+
+ff = open('request.json', encoding='utf8')
+pprint.pprint(post('http://127.0.0.1:5000/post', json=json.load(ff)).json()['response']['text'])
+ff.close()
+print()
+
+ff = open('req_vasya.json', encoding='utf8')
+pprint.pprint(post('http://127.0.0.1:5000/post', json=json.load(ff)).json()['response']['text'])
+ff.close()
+print()
+
+ff = open('req_yes.json', encoding='utf8')
+pprint.pprint(post('http://127.0.0.1:5000/post', json=json.load(ff)).json()['response'])
+ff.close()
+print()
+
+
